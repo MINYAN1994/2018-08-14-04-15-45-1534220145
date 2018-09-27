932e7ee4cb9048ce5c3d753c7bc8935408ff0834
MIN YAN <miny1994@163.com>
2018-09-27 11:50:05 +0800

    calculate the remainder test

--- main/main.js
+++ main/main.js
@@ -1,4 +1,8 @@
 // Write your cade below:
 module.exports = function main() {
-  
+  describe('main()',() => {
+	  it ('should calculate the remainder', () => {
+		 expect(main(10,4)).toBe(2); 
+	  }); 
+	}); 
 };
